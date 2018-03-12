a8d2819e28d68a622107d7c771b45eec1fffe539
pollman <caleb.pollman@zeeram.com>
2018-03-11 21:36:24 -0600

    removed vehicle actions and reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,12 +1,8 @@
 import {combineReducers} from 'redux';
 import {vehicles, vehiclesHasError, vehiclesIsLoading} from './vehicles';
-import {vehicle, vehicleHasError, vehicleIsLoading} from './vehicle';
 
 export default combineReducers({
   vehicles, 
   vehiclesHasError, 
   vehiclesIsLoading,
-  vehicle, 
-  vehicleHasError, 
-  vehicleIsLoading,
 });
